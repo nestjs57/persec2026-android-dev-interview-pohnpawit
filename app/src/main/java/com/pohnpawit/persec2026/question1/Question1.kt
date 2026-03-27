19935dec5afd10811b7d780442889771a6c181e1
nest pohnpawit <seecc0147@gmail.com>
2026-03-27 22:07:31 +0700

    feat: add question1 function and unit tests

--- app/src/main/java/com/pohnpawit/persec2026/question1/Question1.kt
+++ app/src/main/java/com/pohnpawit/persec2026/question1/Question1.kt
@@ -1,9 +1,40 @@
 package com.pohnpawit.persec2026.question1
 
 fun main() {
+    val inputs = listOf(
+        "()",
+        "([]]",
+        "([{}])",
+        "([[{}]]]",
+        ")",
+        "(]}])",
+        "([)]",
+        "{"
+    )
 
+    inputs.forEach { input ->
+        println("$input => ${solveQuestion1(input)}")
+    }
 }
 
-fun solveQuestion1(input : String) : Boolean{
-    return true
+fun solveQuestion1(input: String): Boolean {
+    val brackets = listOf("()", "[]", "{}")
+    val validChars = listOf('(', ')', '[', ']', '{', '}')
+
+    when {
+        input.isEmpty() -> return false
+        input.isBlank() -> return false
+        input.length.mod(2) != 0 -> return false
+        input.any { it !in validChars } -> return false
+    }
+
+    var inputTrim = input.trim()
+
+    while (brackets.any { inputTrim.contains(it) }) {
+        brackets.forEach {
+            inputTrim = inputTrim.replace(it, "")
+        }
+    }
+
+    return inputTrim.isBlank()
 }
--- app/src/test/java/com/pohnpawit/persec2026/Question1Test.kt
+++ app/src/test/java/com/pohnpawit/persec2026/Question1Test.kt
@@ -1,5 +1,44 @@
 package com.pohnpawit.persec2026
 
+import com.pohnpawit.persec2026.question1.solveQuestion1
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
 class Question1Test {
 
+    @Test
+    fun should_return_true_when_brackets_are_valid() {
+        assertEquals(true, solveQuestion1("()"))
+        assertEquals(true, solveQuestion1("([{}])"))
+    }
+
+    @Test
+    fun should_return_false_when_brackets_are_invalid() {
+        assertEquals(false, solveQuestion1("([]]"))
+        assertEquals(false, solveQuestion1("([[{}]]]"))
+        assertEquals(false, solveQuestion1(")"))
+        assertEquals(false, solveQuestion1("(]}])"))
+        assertEquals(false, solveQuestion1("([)]"))
+        assertEquals(false, solveQuestion1("{"))
+    }
+
+    @Test
+    fun should_return_false_when_input_is_blank() {
+        assertEquals(false, solveQuestion1(""))
+    }
+
+    @Test
+    fun should_return_false_when_input_is_empty() {
+        assertEquals(false, solveQuestion1(""))
+    }
+
+    @Test
+    fun should_return_false_when_input_contains_invalid_characters() {
+        assertEquals(false, solveQuestion1("asd"))
+    }
+
+    @Test
+    fun should_return_false_when_input_length_is_odd() {
+        assertEquals(false, solveQuestion1(")"))
+    }
 }
